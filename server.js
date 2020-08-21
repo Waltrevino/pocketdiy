1c5a48c23c955fdb70b761327fa04a27d5794b58
waltrevino <27642702+Waltrevino@users.noreply.github.com>
2020-08-20 20:53:36 -0500

    deploy deployment config

--- server.js
+++ server.js
@@ -15,5 +15,8 @@ app.get("/api/test", function(req, res){
     })
 })
 
+app.get("*", function(req, res){
+    res.sendFile("./client/build/index.html");
+})
 
 app.listen(PORT, () => console.log(`Listening on port: ${PORT}`));
