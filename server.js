876f300c2543365ebb9de23405ad7600b3a10a3a
waltrevino <27642702+Waltrevino@users.noreply.github.com>
2020-08-20 20:47:15 -0500

    added new deployment files

--- server.js
+++ server.js
@@ -5,11 +5,15 @@ const PORT = process.env.PORT || 3001;
 
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
+if (process.env.NODE_ENV === "production"){
+    app.use(express.static("client/build"));
+}
 
 app.get("/api/test", function(req, res){
     res.json({
         test: "test"
     })
 })
+
 
 app.listen(PORT, () => console.log(`Listening on port: ${PORT}`));
